267b7056dff537207a9ff522cae19572bfd233e3
Franco Caraffa <francocaraffa2005@gmail.com>
2023-11-02 20:37:40 -0300

    first server

--- 5.file-system-async-await.js
+++ 5.file-system-async-await.js
@@ -1,16 +1,15 @@
-const fs = require("node:fs/promises");
+const fs = require('node:fs/promises');
 
 (
-  async ()=>{
-    console.log("leyendo el primer archivo");
-    const text = await fs.readFile("./archivo.txt", "utf-8");
-    console.log("Primer texto: ", text);
+  async () => {
+    console.log('leyendo el primer archivo')
+    const text = await fs.readFile('./archivo.txt', 'utf-8')
+    console.log('Primer texto: ', text)
 
-    console.log("Hacer esto mientras lee el archivo");
+    console.log('Hacer esto mientras lee el archivo')
 
-    console.log("leyendo el segundo archivo");
-    const secondText = await fs.readFile("./archivo2.txt", "utf-8");
-    console.log(secondText);
+    console.log('leyendo el segundo archivo')
+    const secondText = await fs.readFile('./archivo2.txt', 'utf-8')
+    console.log(secondText)
   }
 )()
-
