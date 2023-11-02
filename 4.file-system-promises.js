c7dd930574336e8196883e78b52f4757e9b63081
Franco Caraffa <francocaraffa2005@gmail.com>
2023-11-02 13:22:00 -0300

    native modules and async js

---
+++ 4.file-system-promises.js
@@ -0,0 +1,11 @@
+const fs = require("node:fs/promises");
+
+console.log("leyendo el primer archivo");
+fs.readFile("./archivo.txt", "utf-8")
+  .then(text => console.log(text))
+
+console.log("Hacer esto mientras lee el archivo");
+
+console.log("leyendo el segundo archivo");
+fs.readFile("./archivo2.txt", "utf-8")
+  .then(text => console.log(text))
